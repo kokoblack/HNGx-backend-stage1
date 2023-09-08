f9d97954ed9d643d5101cb0f2ea26ba70007a35e
kokoblack <benzimakareem26@gmail.com>
2023-09-08 16:18:05 +0100

    fix: fix endpoint issue

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-const express = require('express')
-var bodyParser = require('body-parser')
+const express = require("express");
+var bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/?slack_name=Kaay&track=backend', (req, res) => {
+// app.get('/', (req, res) => {
+//   res.send('hello!!!!!')
+// })
 
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+app.get("/", (req, res) => {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    const slack_name = req.params('slack_name')
-    const track = req.params('backend')
-    const current_day = weekday[new Date().getDay()]
-    const utc_time = new Date().toUTCString().split('.')[0] + 'z'
-    const github_fle_url = 'https://github.com/kokoblack/HNGx-backend-stage1/blob/main/index.js'
-    const github_repo_url = 'https://github.com/kokoblack/HNGx-backend-stage1'
-    const status_code = 200
+  const slack_name = req.query.slack_name;
+  const track = req.query.track;
+  const current_day = weekday[new Date().getDay()];
+  const utc_time = new Date().toUTCString().split(".")[0] + "z";
+  const github_fle_url =
+    "https://github.com/kokoblack/HNGx-backend-stage1/blob/main/index.js";
+  const github_repo_url = "https://github.com/kokoblack/HNGx-backend-stage1";
+  const status_code = 200;
 
-    const data = {
-        slack_name,
-        current_day,
-        utc_time,
-        track,
-        github_fle_url,
-        github_repo_url,
-        status_code
-    }
+  const data = {
+    slack_name,
+    current_day,
+    utc_time,
+    track,
+    github_fle_url,
+    github_repo_url,
+    status_code,
+  };
 
-  res.send(data)
-})
+  res.send(data);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
